92f47507a3b2d0b34d9557cd9b19063be9866898
Kevin Cóndor <84334368+KevinEC17@users.noreply.github.com>
2025-06-12 20:38:48 -0500

    ADD: puerto y deploy

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
 
-  await app.listen(3000);
+  await app.listen(3007);
   app.enableCors();
 }
 bootstrap();
